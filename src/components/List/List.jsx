6e72f11ad71b62ce7f1ac8f1bb4108372057587c
ppyom <ouob77@kakao.com>
2024-08-30 17:45:25 +0900

    refactor: List 컴포넌트 수정
    * Item의 key를 item.id로 수정

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,8 +4,8 @@ import './List.css';
 const List = ({ list, remove }) => {
   return (
     <ul className="List mw">
-      {list.map((item, idx) => (
-        <Item key={`item_${idx}`} item={item} remove={() => remove(item.id)} />
+      {list.map((item) => (
+        <Item key={item.id} item={item} remove={() => remove(item.id)} />
       ))}
     </ul>
   );
